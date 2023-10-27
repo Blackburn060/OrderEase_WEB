36b11e541b8a3d6cce23a47d980e352685de6847
MarceloTizo <106279700+MarceloTizo@users.noreply.github.com>
2023-10-26 23:30:08 -0300

    Ajustes na PageProduct

--- orderease/src/App.js
+++ orderease/src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <NavigationBar />
+       {/*  <NavigationBar /> */}
         
       </header>
       <body>
--- orderease/src/PageProduct/pageProduct.js
+++ orderease/src/PageProduct/pageProduct.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./PageProduct.css";
+import Images from "../Images/LogoExemplo.png";
 
 function NavigationBar() {
   const [sidebarOpen, setSidebarOpen] = useState(false);
@@ -13,12 +14,10 @@ function NavigationBar() {
     <div>
       <div id="header">
         <div>
-          <span id="menu-toggle" onClick={toggleSidebar}>&#9776;</span>
-          <img
-            id="logo"
-            src="D:\FACULDADE\2023\Web, Mobile\PaginaWeb\Logo.webp"
-            alt="Logo da Empresa"
-          />
+          <span id="menu-toggle" onClick={toggleSidebar}>
+            &#9776;
+          </span>
+          <img id="logo" src={Images} alt="Logo da Empresa" />
           <span id="company-name">OrderEase</span>
           <div id="sidebar" style={{ left: sidebarOpen ? "0px" : "-250px" }}>
             {/* Conteúdo do menu lateral pode ser adicionado aqui */}
@@ -29,14 +28,10 @@ function NavigationBar() {
         </div>
         <span id="user-name">
           Marcelo Tizo
-          <img
-            id="user-photo"
-            src="caminho_da_foto_de_perfil.jpg"
-            alt="Foto de Perfil"
-          />
+          <img id="user-photo" src={Images} alt="Foto de Perfil" />
         </span>
       </div>
-      <h1>garçom</h1>
+
       <div id="footer">Desenvolvido por TizDeveloper Inc.</div>
     </div>
   );
