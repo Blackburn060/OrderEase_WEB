4237f8638e4edc9b9c5bb55c4806ef233fd3f635
MarceloTizo <106279700+MarceloTizo@users.noreply.github.com>
2023-10-31 19:36:09 -0300

    implemetacao da tela de produto

--- orderease/src/Components/PageProduct/pageProduct.js
+++ orderease/src/Components/PageProduct/pageProduct.js
@@ -1,11 +1,28 @@
-import "./PageProduct.css";
+import React from "react";
 import NavigationBar from "../NavigationBar/NavigationBar";
+import "./PageProduct.css";
 
-function pageProduct() {
-
+function PageProduct() {
   return (
-    <NavigationBar />
+    <div className="page">
+      <NavigationBar />
+      <div className="content">
+        <div className="title-container">
+          <h1>Produtos</h1>
+        </div>
+        <div className="ProdutosCadastrados">
+          
+          <div className="NomeDadosProdutos">
+            <div className="top-text">
+              <p>Dados do Produto</p>
+              <p>Nome do Produto</p>
+            </div>
+            <p>Conteúdo da segunda</p>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
-export default pageProduct;
+export default PageProduct;
