53eb0c32c9be423ba24457e9a6eeceede2bdd7c0
Blackburn060 <Blacktauz30@outlook.com>
2023-11-04 02:56:08 -0300

    Correção na tela de login

--- orderease/src/App.js
+++ orderease/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {BrowserRouter as Router, Routes, Route, Navigate,} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate, Outlet } from "react-router-dom";
 import PageHome from "./Components/ComponentPageHome/PageHome";
 import PageProduct from "./Components/ComponentPageProduct/pageProduct";
 import PageMenu from "./Components/ComponentPageMenu/PageMenu";
@@ -13,19 +13,29 @@ import NavigationBar from "./Components/ComponentNavigationBar/NavigationBar";
 function App() {
   return (
     <Router>
-      <NavigationBar />
       <Routes>
         <Route path="/" element={<Navigate to="/home" />} />
-        <Route path="/home" element={<PageHome />} />
-        <Route path="/pageProduct" element={<PageProduct />} />
-        <Route path="/PageMenu" element={<PageMenu />} />
-        <Route path="/PageTables" element={<PageTables />} />
-        <Route path="/PageWaiters" element={<PageWaiters />} />
-        <Route path="/PageRequests" element={<PageRequests />} />
-        <Route path="/PageSettings" element={<PageSettings />} />
+        <Route element={<LayoutWithNavbar />}>
+          <Route path="/home" element={<PageHome />} />
+          <Route path="/pageProduct" element={<PageProduct />} />
+          <Route path="/PageMenu" element={<PageMenu />} />
+          <Route path="/PageTables" element={<PageTables />} />
+          <Route path="/PageWaiters" element={<PageWaiters />} />
+          <Route path="/PageRequests" element={<PageRequests />} />
+          <Route path="/PageSettings" element={<PageSettings />} />
+        </Route>
         <Route path="/PageLogin" element={<PageLogin />} />
       </Routes>
     </Router>
+  );
+}
+
+function LayoutWithNavbar() {
+  return (
+    <div>
+      <NavigationBar />
+      <Outlet />
+    </div>
   );
 }
 
