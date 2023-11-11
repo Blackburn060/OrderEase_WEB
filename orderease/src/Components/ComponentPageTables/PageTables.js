a222728a40882a7ec26d9adaa0f186387e502d53
MarceloTizo <106279700+MarceloTizo@users.noreply.github.com>
2023-11-11 12:28:31 -0300

    Insercao da pagina de Mesas

--- orderease/src/Components/ComponentPageTables/PageTables.js
+++ orderease/src/Components/ComponentPageTables/PageTables.js
@@ -1,8 +1,26 @@
+import React from "react";
+import "./PageTables.css";
 function PageTables() {
+  return (
+    <div className="page">
+      <div className="contentTable">
+        <div className="titleContainerTable">
+          <h1>Mesas</h1>
+        </div>
+        <div className="ButtonNewTable">
+          <button>Novo</button>
+        </div>
+        <div className="DetailedTableData">
+          <div className="DetailedTableDataTittle">
+            <h2>Dados das Mesas - </h2>
+          </div>
+          <div className="DetailedTableDataContent">
+            {/* Seu conteúdo aqui */}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
 
-    return (
-      <></>
-    );
-  }
-  
-  export default PageTables;
+export default PageTables;
