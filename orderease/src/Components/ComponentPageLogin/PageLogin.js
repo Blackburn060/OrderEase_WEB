e36bb106891da5800a2829595824e8acb0ff5cff
Blackburn060 <Blacktauz30@outlook.com>
2023-11-27 01:50:56 -0300

    Implementação de autenticação de usuário

--- orderease/src/Components/ComponentPageLogin/PageLogin.js
+++ orderease/src/Components/ComponentPageLogin/PageLogin.js
@@ -1,9 +1,49 @@
 import React, { useState, useEffect } from "react";
+import { Navigate } from "react-router-dom";
 import "./PageLogin.css";
 import IconUser from "../../assets/Images/IconeUserLoginPage.png";
 import IconPassword from "../../assets/Images/IconeCadeadoLogin.png";
+import { initializeApp } from "firebase/app";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDkm_pw2kGzze_T800gJYq2HMUYnY49Qx4",
+  authDomain: "orderease-76588.firebaseapp.com",
+  databaseURL: "https://orderease-76588-default-rtdb.firebaseio.com",
+  projectId: "orderease-76588",
+  storageBucket: "orderease-76588.appspot.com",
+  messagingSenderId: "625191016976",
+  appId: "1:625191016976:web:a06ee20338f6089c6dc895",
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
 
 function PageLogin() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+  const [redirect, setRedirect] = useState(false);
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleLogin = async () => {
+    const auth = getAuth(firebaseApp);
+
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      console.log("Login bem-sucedido!");
+      setRedirect(true);
+    } catch (error) {
+      console.error("Erro ao fazer login:", error.message);
+      setError(error.message);
+    }
+  };
 
   const [companyInfo, setCompanyInfo] = useState({
     loginPageUrl: "",
@@ -15,19 +55,29 @@ function PageLogin() {
 
   const fetchCompanyInfo = async () => {
     try {
-      const response = await fetch("https://orderease-api.onrender.com/api/obter-configuracoes");
+      const response = await fetch(
+        "https://orderease-api.onrender.com/api/obter-configuracoes"
+      );
       if (response.ok) {
         const data = await response.json();
         setCompanyInfo({
           loginPageUrl: data.loginPageImage,
         });
       } else {
-        console.error("Erro ao obter informações da empresa:", response.statusText);
+        console.error(
+          "Erro ao obter informações da empresa:",
+          response.statusText
+        );
       }
     } catch (error) {
       console.error("Erro durante a solicitação para o servidor:", error);
     }
   };
+
+  if (redirect) {
+    // Redirecionar para a página inicial
+    return <Navigate to="/home" />;
+  }
 
   return (
     <div className="container">
@@ -46,17 +96,28 @@ function PageLogin() {
             <i className="icon">
               <img src={IconUser} alt="Icone Usuario"></img>
             </i>
-            <input type="text" placeholder="Usuário"></input>
+            <input
+              type="email"
+              placeholder="E-mail"
+              value={email}
+              onChange={handleEmailChange}
+            ></input>
           </div>
           <div className="form-group">
             <i className="icon">
               <img src={IconPassword} alt="Icone Senha"></img>
             </i>
-            <input type="password" placeholder="Senha"></input>
+            <input
+              type="password"
+              placeholder="Senha"
+              value={password}
+              onChange={handlePasswordChange}
+            ></input>
           </div>
           <div className="form-group">
-            <button>ENTRAR</button>
+            <button onClick={handleLogin}>ENTRAR</button>
           </div>
+          {error && <p className="error-message">Falha no login! E-mail ou senha incorretos!</p>}
         </div>
       </div>
     </div>
