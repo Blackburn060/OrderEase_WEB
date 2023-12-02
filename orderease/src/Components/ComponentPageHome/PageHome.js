fe32f94336d09994047ef7380638b4679e69a652
Blackburn060 <Blacktauz30@outlook.com>
2023-12-02 11:49:12 -0300

    Correção de responsividade page home

--- orderease/src/Components/ComponentPageHome/PageHome.js
+++ orderease/src/Components/ComponentPageHome/PageHome.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import "./PageHome.css";
-import ImageUserLogo from "../../assets/Images/LogoExemplo.png";
 
 function PageHome() {
   const [companyInfo, setCompanyInfo] = useState({
