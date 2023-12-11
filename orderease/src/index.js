6f121c174f298def9995e5c52ddadfa622fdcc84
Blackburn060 <Blacktauz30@outlook.com>
2023-12-10 23:33:37 -0300

    Implementação de persistência de autenticação

--- orderease/src/index.js
+++ orderease/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { AuthProvider } from './context/AuthContext';
 
 fetch("https://orderease-api.up.railway.app/api/obter-configuracoes")
   .then((response) => response.json())
@@ -25,7 +26,9 @@ fetch("https://orderease-api.up.railway.app/api/obter-configuracoes")
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+        <App />
+      </AuthProvider>
   </React.StrictMode>
 );
 
